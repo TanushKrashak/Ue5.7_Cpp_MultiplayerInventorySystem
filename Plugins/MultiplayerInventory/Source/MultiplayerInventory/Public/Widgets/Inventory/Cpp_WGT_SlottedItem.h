d3fc93cb80a5f3572287bed414122d40d55b39cf
TanushKrashak <tankrashak38816@gmail.com>
2026-07-07 20:53:24 +0530

    created base slotted widget

---
+++ Plugins/MultiplayerInventory/Source/MultiplayerInventory/Public/Widgets/Inventory/Cpp_WGT_SlottedItem.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Cpp_WGT_SlottedItem.generated.h"
+
+class UCpp_InventoryItem;
+// Forward Declarations
+class UImage;
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERINVENTORY_API UCpp_WGT_SlottedItem : public UUserWidget {
+	GENERATED_BODY()
+	
+public:
+	//=================================================================================================================
+	// FUNCTIONS
+	//=================================================================================================================
+	FORCEINLINE UImage* GetImageIcon() const { return IMG_Icon; }
+	
+	FORCEINLINE void SetGridIndex(const int32 Index) { GridIndex = Index; }
+	FORCEINLINE int32 GetGridIndex() const { return GridIndex; }
+	FORCEINLINE void SetGridDimensions(const FIntPoint& Dimensions) { GridDimensions = Dimensions; }
+	FORCEINLINE FIntPoint GetGridDimensions() const { return GridDimensions; }
+	
+	FORCEINLINE void SetIsStackable(const bool bStackable) { bIsStackable = bStackable; }
+	
+
+private:
+	//=================================================================================================================
+	// PROPERTIES & VARIABLES
+	//=================================================================================================================
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> IMG_Icon;
+	
+	FIntPoint GridDimensions;
+	int32 GridIndex;
+	
+	TWeakObjectPtr<UCpp_InventoryItem> InventoryItem;
+	
+	bool bIsStackable = false;
+};
